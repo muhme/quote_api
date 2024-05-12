4bdaf00f6d88bd4844c13b6a31ef00875edc76ec
muhme <github@heikol.de>
2024-05-12 11:43:49 +0200

    wait with test until node is running

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,4 +6,8 @@
 # OpenAPI api.zitat-service.de, see https://github.com/muhme/quote_api
 
 echo "*** quote_api_nodeapp:npm run test"
+# Importand to wait that node is running. As test is doing rebuild and
+#   if this runs before the node server is running it destroys the base,
+#   node will fail and the container stops and is no more restartable.
+node_modules/wait-on/bin/wait-on -l -t "60s" "http://localhost:3000"
 docker exec -it quote_api_nodeapp /usr/local/bin/npm run test
